5b5f1f041ac504a4c3116bcdbf13ef022b1d61be
jearbear1 <onwubuyajeremiah@gmail.com>
2023-07-05 22:55:33 -0400

    obcopy and dump change
    add avr-gcc@12
    
    include dacc.c
    
    dac includes and channel select fix

--- platforms/atsam4s/api/dac.c
+++ platforms/atsam4s/api/dac.c
@@ -1,25 +1,32 @@
 #include "libflipper.h"
-#include <dacc.h> 
 #include "sam4s16b.h"
+#include <sysclk.h> 
+#include <pmc.h> 
+#include <dacc.h> 
+#include <dacc.c> 
+
+int dac_configure(void);
+int dac_enbale_channels(int);
+int dac_set_channels(int);
+int dac_write(void);
+int dac_cleanup(void);
+
 
 LF_FUNC int dac_configure(void) {
 
     // 12 bit DAC
     // initialize system clock 
-    //sysclk_init();
-   
+    sysclk_init();
 
     // Enable the Peripheral clock 
     // DAC Peripheral ID = 30 
-    //sysclk_enable_peripheral_clock(30);
-    pmc_enable_periph_clk(DACC);
+    sysclk_enable_peripheral_clock(30);
 
     // Reset the DAC 
     dacc_reset(DACC);
 
     // Enable the DAC 
-    //dacc_enable(DACC);
-
+    dacc_enable_flexible_selection(DACC);
 
     // Set the timing for the DAC
     dacc_set_timing(DACC, DACC_MR_MAXS_NORMAL, DACC_MR_STARTUP_1920);
@@ -38,37 +45,37 @@ LF_FUNC int dac_configure(void) {
  
 }
 
-LF_FUNC int dac_enbale_channel0(void) {
+LF_FUNC int dac_enbale_channels(int i) {
 
-    // Enaable Digital to Analog Conversion channel 0
-    dacc_enable_channel(DACC, DACC_MR_USER_SEL_CHANNEL0); 
-
+    if (i == 0) {
+        // Enaable Digital to Analog Conversion channel 0
+        dacc_enable_channel(DACC, DACC_MR_USER_SEL_CHANNEL0); 
     return lf_success; 
+  } else if (i == 1) { 
+        // Enaable Digital to Analog Conversion channel 1
+        dacc_enable_channel(DACC, DACC_MR_USER_SEL_CHANNEL1); 
+    return lf_success;
+  } else {
+    return lf_error;
+  }
+
 }
 
-LF_FUNC int dac_set_channel0(void) {
+// Set Digital to Analog Conversion channels
 
-    // set Digital to Analog Conversion channel to channel 0
-    dacc_set_channel_selection(DACC, DACC_MR_USER_SEL_CHANNEL0); 
-
-   return lf_success; 
-}
-
-
-LF_FUNC int dac_enbale_channel1(void) {
-
-    // Enaable Digital to Analog Conversion channel 1
-    dacc_enable_channel(DACC, DACC_MR_USER_SEL_CHANNEL1); 
-
+LF_FUNC int dac_set_channels(int i) {
+    if (i == 0) {
+        // set Digital to Analog Conversion channel to channel 0
+        dacc_set_channel_selection(DACC, DACC_MR_USER_SEL_CHANNEL0);
     return lf_success; 
-}
+  } else if (i == 1) { 
+        // set Digital to Analog Conversion channel to channel 1
+        dacc_set_channel_selection(DACC, DACC_MR_USER_SEL_CHANNEL1); 
+    return lf_success;
+  } else {
+    return lf_error;
+  }
 
-LF_FUNC int dac_set_channel1(void) {
-
-    // set Digital to Analog Conversion channel to channel 1
-    dacc_set_channel_selection(DACC, DACC_MR_USER_SEL_CHANNEL1); 
-
-   return lf_success; 
 }
 
 LF_FUNC int dac_write(void) {
