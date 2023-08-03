7aefcd8b57ace717d564017928806bf47f7d80ae
jearbear1 <onwubuyajeremiah@gmail.com>
2023-08-03 08:41:58 -0400

    i2C setup

--- platforms/atsam4s/api/i2c.c
+++ platforms/atsam4s/api/i2c.c
@@ -1,27 +1,87 @@
 #include "libflipper.h"
 #include <twi.h>
+#include "sam4s16b.h"
+#include <sysclk.h> 
+#include <pmc.h> 
+
+void mastrClkConfig(int x);
+void speedConfig(int x);
+void chipConfig(int x);
+void smbusConfig(int x);
+
+
+twi_options_t twi_opt;
+twi_packet_t packet;
+
+LF_FUNC masterEnable(void) {
+    twi_enable_master_mode(TWI0);
+}
+
+LF_FUNC void mastrClkConfig(int x) {
+    /*
+        // MCK for TWI.
+        twi_opt.master_clk = x;
+    */
+}
+
+LF_FUNC void speedConfig(int x) {
+    //! The baud rate of the TWI bus.
+    twi_opt.speed = x;
+}
+
+LF_FUNC void chipConfig(int x) { 
+    // The desired address.
+    twi_opt.chip = x;
+}
+
+LF_FUNC void smbusConfig(int x) { 
+    // SMBUS mode (set 1 to use SMBUS quick command, otherwise don't).
+    twi_opt.smbus = 1;
+}
 
 LF_FUNC int i2c_configure(void) {
+
+    /*
+    TWD and TWCK pins may be multiplexed with PIO lines.
+    To enable the TWI, the user must program the PIO Controller 
+    to dedicate TWD and TWCK as peripheral lines.
+    */
+
+   /*
+    Two Wire Interface 0 (TWI0)
+    ID_TWI0   (19)
+    Two Wire Interface 1 (TWI1) 
+    ID_TWI1   (20) 
+    */
+
+    twi_master_init(TWI0, &twi_opt);
+    sysclk_enable_peripheral_clock(19);
     return lf_success;
 }
 
-/* Starts a read session. */
+// Starts a read session. 
 LF_FUNC void i2c_start_read(uint8_t address, uint8_t length) {
+
+    twi_master_read(TWI0, &packet);
 }
 
-/* Contiunues reading once a read session has begun. */
+// Contiunues reading once a read session has begun. 
 LF_FUNC uint8_t i2c_read(void) {
     return 0;
 }
 
-/* Starts a write session. */
+// Starts a write session. 
 LF_FUNC void i2c_start_write(uint8_t address, uint8_t length) {
+
+    twi_master_write(TWI0, &packet);
 }
 
-/* Continues writing once a write session has begun. */
+// Continues writing once a write session has begun. 
 LF_FUNC void i2c_write(uint8_t byte) {
 }
 
-/* Stops the active session. */
+// Stops the active session. 
 LF_FUNC void i2c_stop(void) {
+    twi_disable_master_mode(TWI0);
 }
+
--- platforms/atsam4s/api/uart0.c
+++ platforms/atsam4s/api/uart0.c
@@ -1,160 +1,40 @@
 #include "libflipper.h"
-#include "sam4s16b.h"
-#include <sysclk.h> 
-#include <pmc.h> 
-#include <uart.h> 
-
-int uart0_configure(void);
-int uart0_reset(void);
-int uart0_ready(void);
-int uart0_enable(void); 
-int uart0_disable(void);
-uint8_t uart0_get(void); 
-int uart0_write(void);
-int uart0_read(void);
-int uart0_get_uart_interrupt_mask(void);
-const uint8_t uc_data;
-uint8_t *puc_data;
-int changeBaudRate(int);
-int changeMode(int);
-
-/*
-    
-The baud rate clock is the peripheral clock divided by 16 times the clock divisor (CD)
-value written in the Baud Rate Generator register (UART_BRGR). 
-If UART_BRGR is set to 0, the baud rate clock is disabled and the UART remains inactive. 
-The maximum allowable baud rate is peripheral clock divided by 16. 
-The minimum allowable baud rate is peripheral clock divided by (16 x 65536).
-
-The Automatic echo mode allows a bit-by-bit retransmission. 
-When a bit is received on the URXD line, it is sent to the UTXD line.
-The transmitter operates normally, but has no effect on the UTXD line.
-
-The Local loopback mode allows the transmitted characters to be received. 
-UTXD and URXD pins are not used and the output of the transmitter is internally connected to the input of the receiver. 
-The URXD pin level has no effect and the UTXD line is held high, as in idle state.
-
-The Remote loopback mode directly connects the URXD pin to the UTXD line. 
-The transmitter and the receiver are disabled and have no effect. 
-This mode allows a bit-by-bit retransmission.
-
-0: Normal mode
-1: Automatic echo
-2: Local loopback
-3: Remote loopback
-*/
-
-sam_uart_opt_t uart0_opt;
+#include <uart.h>
 
 LF_FUNC int uart0_configure(void) {
 
-    // Enable the Peripheral clock 
-    // UART0 Peripheral ID = 8 
-    pmc_enable_periph_clk(ID_UART0);
-
-    // configure uart options struct
-    // 120 MHz Master Clock, 9600 Baudrate, Normal Mode
-    uart0_opt.ul_mck = 120;
-    uart0_opt.ul_baudrate = 9600;
-    uart0_opt.ul_mode = 0;
-
-    uart_init(UART0, &uart0_opt);
-    // UART0_IRQn = 8
-    uart_enable_interrupt(UART0, UART0_IRQn);
-
     return lf_success;
 }
 
-LF_FUNC int uart0_enable(void) {
-    // Enable UART
-    uart_enable(UART0);
+LF_FUNC int uart0_setbaud(uint32_t baud) {
     return lf_success;
-}
-
-LF_FUNC int uart0_disable(void) {
-
-    // Disable UART receiver.
-    uart_disable_rx(UART0);
-    // Disable UART transmitter.
-    uart_disable_tx(UART0);
-    // Disable UART Interrupt UART0_IRQn = 8
-    uart_disable_interrupt(UART0, UART0_IRQn);
-    // Disable UART Peripheral
-    uart_disable(UART0);
-   
-     return lf_success;
 }
 
 LF_FUNC int uart0_reset(void) {
-    // reset transmitter receiver and uart status
-    uart_reset_status(UART0);
-    uart_reset(UART0);
     return lf_success;
 }
 
 LF_FUNC int uart0_ready(void) {
-    uart_is_rx_ready(UART0);
-    uart_is_tx_ready(UART0);
-    return lf_success;
+    return 0;
 }
 
-LF_FUNC uint8_t uart0_get(void) {
-    // get UART status
-    uart_get_status(UART0);
-    return lf_success;
+LF_FUNC void uart0_enable(void) {
 }
 
-LF_FUNC int uart0_write(void) {
-    uart_write(UART0, uc_data);
-    return lf_success;
+LF_FUNC void uart0_disable(void) {
 }
 
-LF_FUNC int uart0_read(void) {
-    uart_read(UART0, puc_data);
-    return lf_success;
-}
-
-
-LF_FUNC int uart0_get_uart_interrupt_mask(void) { 
-    uart_get_interrupt_mask(UART0);
-    return lf_success;
-}
-
-LF_FUNC int changeBaudRate(int x) {
-    /*
-    if:
-        (x > peripheral clock divided by (16 x 65536)
-        &&
-        (x < peripheral clock divided by 16 )
-        then 
-        uart0_opt.ul_baudrate = x;
-        return lf_success;
-    */
-    return lf_success;
-}
-
-LF_FUNC int changeMode(int x) {
-
-    if (x == 0) {
-        uart0_opt.ul_mode = 0;
-        return lf_success;
-    } else if (x == 1) {
-        uart0_opt.ul_mode = 1;
-        return lf_success;
-    } else if (x == 2) {
-        uart0_opt.ul_mode = 2;
-        return lf_success;
-    } else if (x == 3) {
-        uart0_opt.ul_mode = 3;
-        return lf_success;
-    } else {
-        return lf_error; 
-    }
-
-    return lf_success;
-}
-
-// DO NOT DELETE THIS FUNCTION
 LF_FUNC void uart0_put(uint8_t byte) {
 }
 
+LF_FUNC uint8_t uart0_get(void) {
+    return 0;
+}
+
+LF_FUNC int uart0_write(void *src, uint32_t length) {
+    return lf_success;
+}
+
+LF_FUNC int uart0_read(void *dst, uint32_t length) {
+    return lf_success;
+}
