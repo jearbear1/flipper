4f0e4e3944d06b1911e07bc61a587348bc910373
jearbear1 <onwubuyajeremiah@gmail.com>
2023-09-04 03:19:57 -0400

    GPIO finished

--- platforms/atsam4s/api/gpio.c
+++ platforms/atsam4s/api/gpio.c
@@ -12,6 +12,17 @@ int gpioSetOutput(uint32_t x);
 int gpioSetInput(uint32_t x);
 void gpio_write(uint32_t set, uint32_t clear);
 uint32_t gpio_read(uint32_t mask);
+int configureInterruptRiseEdge(uint32_t x);
+int configureInterruptFallEdge(uint32_t x);
+int getInterruptMask(const Pio *p_pio);
+int getInterruptStatus(const Pio *p_pio);
+int disableInterrupt(uint32_t x);
+void getOutputDataStatus(const Pio *p_pio, const uint32_t ul_mask);
+void setSchmittTrigger(Pio *p_pio, const uint32_t ul_mask);
+void getSchmittTrigger(const Pio *p_pio);
+int captureSetMode(int x);
+void captureEnable(Pio *p_pio);
+void captureDisable(Pio *p_pio);
 
 
 LF_FUNC int gpioConfigure(void) {
@@ -20,52 +31,29 @@ LF_FUNC int gpioConfigure(void) {
     sysclk_enable_peripheral_clock(ID_PIOA);
     sysclk_enable_peripheral_clock(ID_PIOB);
     // Clear GPIO signals
-    // Clear IO_1
-    pio_clear(PIOA, PIO_PA22);
-    // Clear IO_2
-    pio_clear(PIOA, PIO_PA23);
-    // Clear IO_3
-    pio_clear(PIOA, PIO_PA2);
-    // Clear IO_4
-    pio_clear(PIOA, PIO_PA30);
-    // Clear IO_5
-    pio_clear(PIOA, PIO_PA29);
-    // Clear IO_6
-    pio_clear(PIOA, PIO_PA28);
-    // Clear IO_7
-    pio_clear(PIOA, PIO_PA27);
-    // Clear IO_8
-    pio_clear(PIOA, PIO_PA7);
-    // Clear IO_9
-    pio_clear(PIOA, PIO_PA8);
-    // Clear IO_10
-    pio_clear(PIOA, PIO_PA26);
-    // Clear IO_11
-    pio_clear(PIOA, PIO_PA25);
-    // Clear IO_12
-    pio_clear(PIOA, PIO_PA24);
-    // Clear IO_14
-    pio_clear(PIOA, PIO_PA16);
-    // Clear IO_15
-    pio_clear(PIOB, PIO_PB7);
-    // Clear IO_16
-    pio_clear(PIOB, PIO_PB6);
-    // clear IO_A1
-     pio_clear(PIOA, PIO_PA20X1_AD3);
-    // clear IO_A2
-     pio_clear(PIOA, PIO_PA19X1_AD2);
-    // clear IO_A3
-     pio_clear(PIOA, PIO_PA18X1_AD1);
-    // clear IO_A4
-     pio_clear(PIOA, PIO_PA17X1_AD0);
-    // clear IO_A5
-     pio_clear(PIOB, PIO_PB3X1_AD7);
-    // clear IO_A6
-     pio_clear(PIOB, PIO_PB2X1_AD6);
-    // clear IO_A7
-     pio_clear(PIOB, PIO_PB1X1_AD5);
-    // clear IO_A8
-     pio_clear(PIOB, PIO_PB0X1_AD4);
+    pio_clear(PIOA, IO_1);
+    pio_clear(PIOA, IO_2);
+    pio_clear(PIOA, IO_3);
+    pio_clear(PIOA, IO_4);
+    pio_clear(PIOA, IO_5);
+    pio_clear(PIOA, IO_6);
+    pio_clear(PIOA, IO_7);
+    pio_clear(PIOA, IO_8);
+    pio_clear(PIOA, IO_9);
+    pio_clear(PIOA, IO_10);
+    pio_clear(PIOA, IO_11);
+    pio_clear(PIOA, IO_12);
+    pio_clear(PIOA, IO_14);
+    pio_clear(PIOB, IO_15);
+    pio_clear(PIOB, IO_16);
+    pio_clear(PIOA, IO_A1);
+    pio_clear(PIOA, IO_A2);
+    pio_clear(PIOA, IO_A3);
+    pio_clear(PIOA, IO_A4);
+    pio_clear(PIOB, IO_A5);
+    pio_clear(PIOB, IO_A6);
+    pio_clear(PIOB, IO_A7);
+    pio_clear(PIOB, IO_A8);
     return lf_success;
 }
 
@@ -87,14 +75,19 @@ LF_FUNC int gpioSetOutput(uint32_t x) {
 }
 
 LF_FUNC int gpioSetInput(uint32_t x) {
-  if (x == (IO_1) || (IO_2) || (IO_3) || (IO_4) || (IO_5) || (IO_6) || (IO_7) || (IO_8) 
-        || (IO_9) || (IO_10) || (IO_11) || (IO_12) || (IO_14) || (IO_15) || (IO_16)
-        || (IO_A1) || (IO_A2) || (IO_A3) || (IO_A4) || (IO_A5) || (IO_A6) || (IO_A7) || (IO_A8)) 
+    if (x == (IO_1) || (IO_2) || (IO_3) || (IO_4) || (IO_5) || (IO_6) || (IO_7) || (IO_8) 
+        || (IO_9) || (IO_10) || (IO_11) || (IO_12) || (IO_14) || (IO_A1) || (IO_A2)
+        || (IO_A3) || (IO_A4)) 
     {
         // pins are set to pio_pullup by defualt.
         pio_set_input(PIOA, x, PIO_PULLUP);
         return lf_success;  
-    }  else {
+
+    } else  if (x == (IO_15) || (IO_16) || (IO_A5) || (IO_A6) || (IO_A7) || (IO_A8)) {
+
+        pio_set_input(PIOB, x, PIO_PULLUP);
+        return lf_success;
+    } else {
         return lf_error;
     }
 }
@@ -106,29 +99,116 @@ LF_FUNC void gpio_write(uint32_t set, uint32_t clear) {
 }
 
 LF_FUNC uint32_t gpio_read(uint32_t mask) {
-    pio_get(PIOA, PIO_TYPE_PIO_INPUT, mask);
-    pio_get(PIOB, PIO_TYPE_PIO_INPUT, mask);
-    return 0;
+    if (mask == (IO_1) || (IO_2) || (IO_3) || (IO_4) || (IO_5) || (IO_6) || (IO_7) || (IO_8) 
+        || (IO_9) || (IO_10) || (IO_11) || (IO_12) || (IO_14) || (IO_A1) || (IO_A2)
+        || (IO_A3) || (IO_A4)) 
+    {
+        pio_get(PIOA, PIO_TYPE_PIO_INPUT, mask);
+        return lf_success;  
+
+    } else  if (mask == (IO_15) || (IO_16) || (IO_A5) || (IO_A6) || (IO_A7) || (IO_A8)) {
+
+        pio_get(PIOB, PIO_TYPE_PIO_INPUT, mask);
+        return lf_success;
+    } else {
+        return lf_error;
+    }
+ 
 }
 
+LF_FUNC int configureInterruptRiseEdge(uint32_t x) {
 
+    if (x == (IO_1) || (IO_2) || (IO_3) || (IO_4) || (IO_5) || (IO_6) || (IO_7) || (IO_8) 
+        || (IO_9) || (IO_10) || (IO_11) || (IO_12) || (IO_14)) 
+    {
+        pio_configure_interrupt(PIOA, x, PIO_IT_RISE_EDGE);
+        pio_enable_interrupt(PIOA, x);
+        return lf_success;  
+    }  else  if (x == (IO_15) || (IO_16)) {
+        pio_configure_interrupt(PIOB, x, PIO_IT_RISE_EDGE);
+        pio_enable_interrupt(PIOB, x);
+        return lf_success;  
+    } else {
+        return lf_error;
+    }
+}
 
-/*
-void pio_set_schmitt_trigger(Pio *p_pio, const uint32_t ul_mask);
-uint32_t pio_get_schmitt_trigger(const Pio *p_pio);
+LF_FUNC int configureInterruptFallEdge(uint32_t x) {
 
-void pio_configure_interrupt(Pio *p_pio, const uint32_t ul_mask,
-		const uint32_t ul_attr);
-void pio_enable_interrupt(Pio *p_pio, const uint32_t ul_mask);
-void pio_disable_interrupt(Pio *p_pio, const uint32_t ul_mask);
-uint32_t pio_get_interrupt_status(const Pio *p_pio);
-uint32_t pio_get_interrupt_mask(const Pio *p_pio);
+    if (x == (IO_1) || (IO_2) || (IO_3) || (IO_4) || (IO_5) || (IO_6) || (IO_7) || (IO_8) 
+        || (IO_9) || (IO_10) || (IO_11) || (IO_12) || (IO_14)) 
+    {
+        pio_configure_interrupt(PIOA, x, PIO_IT_FALL_EDGE);
+        pio_enable_interrupt(PIOA, x);
+        return lf_success;  
+    }  else  if (x == (IO_15) || (IO_16)) {
+        pio_configure_interrupt(PIOB, x, PIO_IT_FALL_EDGE);
+        pio_enable_interrupt(PIOB, x);
+        return lf_success;  
+    } else {
+        return lf_error;
+    }
+}
 
-void pio_capture_set_mode(Pio *p_pio, uint32_t ul_mode);
-void pio_capture_enable(Pio *p_pio);
-void pio_capture_disable(Pio *p_pio);
+LF_FUNC int getInterruptMask(const Pio *p_pio) {
+    pio_get_interrupt_mask(p_pio);
+    return lf_success;
+}
 
-pio_get_output_data_status
+LF_FUNC int getInterruptStatus(const Pio *p_pio) {
+    pio_get_interrupt_status(p_pio);
+    return lf_success;
+}
 
-AX pin are the pins which are used for only reading analog input
-*/
+LF_FUNC int disableInterrupt(uint32_t x) {
+
+    if (x == (IO_1) || (IO_2) || (IO_3) || (IO_4) || (IO_5) || (IO_6) || (IO_7) || (IO_8) 
+        || (IO_9) || (IO_10) || (IO_11) || (IO_12) || (IO_14)) 
+    {
+        pio_disable_interrupt(PIOA, x);
+        return lf_success;  
+    }  else  if (x == (IO_15) || (IO_16)) {
+        pio_disable_interrupt(PIOB, x);
+        return lf_success;  
+    } else {
+        return lf_error;
+    }
+}
+
+LF_FUNC void getOutputDataStatus(const Pio *p_pio, const uint32_t ul_mask) {
+    pio_get_output_data_status(p_pio, ul_mask);		
+}
+ 
+LF_FUNC void setSchmittTrigger(Pio *p_pio, const uint32_t ul_mask) {
+    pio_set_schmitt_trigger(p_pio, ul_mask);
+}
+
+LF_FUNC void getSchmittTrigger(const Pio *p_pio) {
+    pio_get_schmitt_trigger(p_pio);
+}
+
+LF_FUNC int captureSetMode(int x) {
+    if (x == 1) {
+        // Always Sampling 
+        pio_capture_set_mode(PIOA, PIO_PCMR_ALWYS);
+        return lf_success; 
+    } else if(x == 2) {
+        // Half Sampling 
+        pio_capture_set_mode(PIOA, PIO_PCMR_HALFS);
+        return lf_success; 
+    } else if ( x == 3) {
+        // First Sample
+        pio_capture_set_mode(PIOA, PIO_PCMR_FRSTS);
+        return lf_success; 
+    } else {
+        return lf_error;
+    }
+}
+
+LF_FUNC void captureEnable(Pio *p_pio) {
+    pio_capture_enable(p_pio);
+}
+
+LF_FUNC void captureDisable(Pio *p_pio) {
+    pio_capture_disable(p_pio);
+}
