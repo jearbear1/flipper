af704d631f4856492b1e28253ff3be60607740f9
jearbear1 <onwubuyajeremiah@gmail.com>
2023-09-19 00:15:46 -0400

    SPI Setup

--- platforms/atsam4s/api/pwm.c
+++ platforms/atsam4s/api/pwm.c
@@ -1,24 +1,6 @@
 #include "libflipper.h"
 #include <pwm.h>
-#include <sysclk.h> 
-#include "atsam4s.h"
-#include "sam4s16b.h"
-
-pwm_channel_t pwm_channel_instance;
-pwm_clock_t clock_setting;
 
 LF_FUNC int pwm_configure(void) {
-
-    pmc_enable_periph_clk(ID_PWM);
-    clock_setting.ul_clka = 1000 * 100;
-    clock_setting.ul_clkb = 0;
-    clock_setting.ul_mck = 48000000;
-    pwm_channel_enable(PWM, PWM_CHANNEL_0);
-    pwm_init(PWM, &clock_setting);
-    pwm_channel_instance.ul_prescaler = PWM_CMR_CPRE_CLKA;
-    pwm_channel_instance.ul_period = 100;
-    pwm_channel_instance.ul_duty = 50;
-    pwm_channel_instance.channel = PWM_CHANNEL_0;
-    pwm_channel_init(PWM, &pwm_channel_instance);
     return lf_success;
 }
--- platforms/atsam4s/api/spi.c
+++ platforms/atsam4s/api/spi.c
@@ -1,16 +1,47 @@
 #include "libflipper.h"
-//#include <spi.h>
+#include <spi.h>
 
-LF_FUNC int spi_configure() {
+int spi_configure(Spi *p_spi);
+int setSpiMode(Spi *p_spi, int x);
+void enableSpi(Spi *p_spi); 
+void disableSpi(Spi *p_spi);
+uint8_t spi_ready(void) ;
+void spi_end(void); 
+void spiPut(Spi *p_spi, uint16_t data); 
+int spiGet(Spi *p_spi); 
+void spi_isr(void);
 
+
+LF_FUNC int spi_configure(Spi *p_spi) {
+    spi_enable_clock(p_spi);
+    spi_reset(p_spi);
+    spi_disable_mode_fault_detect(p_spi);
+    spi_disable_loopback(p_spi);
+    spi_set_fixed_peripheral_select(p_spi);
+    spi_disable_peripheral_select_decode(p_spi);
     return lf_success;
 }
 
-LF_FUNC void spi_enable(void) {
+LF_FUNC void enableSpi(Spi *p_spi) {
+    spi_enable(p_spi);
 }
 
-LF_FUNC void spi_disable(void) {
+LF_FUNC void disableSpi(Spi *p_spi) {
+    spi_disable(p_spi);
 }
+
+LF_FUNC int setSpiMode(Spi *p_spi, int x) {
+    if (x == 1) {
+        spi_set_master_mode(p_spi);
+        return lf_success;
+    } else if (x == 2) {
+        spi_set_slave_mode(p_spi);
+        return lf_success;
+    } else {
+        return lf_error;
+    }
+}
+
 
 LF_FUNC uint8_t spi_ready(void) {
     return 0;
@@ -19,10 +50,11 @@ LF_FUNC uint8_t spi_ready(void) {
 LF_FUNC void spi_end(void) {
 }
 
-LF_FUNC void spi_put(uint8_t byte) {
+LF_FUNC void spiPut(Spi *p_spi, uint16_t data) {
 }
 
-LF_FUNC uint8_t spi_get(void) {
+LF_FUNC int spiGet(Spi *p_spi) {
+    spi_get_mode(p_spi);
     return 0;
 }
 
@@ -39,5 +71,5 @@ LF_FUNC int spi_read(void *dst, uint32_t length) {
 */
 
 /* interrupt handler for spi */
-void spi_isr(void) {
+LF_FUNC void spi_isr(void) {
 }
